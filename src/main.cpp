3710b9049e4536ee849d6081b85ca1189a06fc50
Farzin Golkhosh <farzin.golkhosh@gmail.com>
2020-06-04 14:02:04 -0700

    [FEAT] Update example in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,18 @@
-#include "../include/session.h"
-#include "../include/node.h"
-#include "../include/operation.h"
+#include "../include/NN.h"
 #include <iostream>
 
 int main()
 {
-    Variable<int> A(1);
-    Variable<int> B(2);
+    NN nn = NN();
+    //Variable<int> A = NN.variable(1);
+    //Variable<int> B = NN.variable(2);
     //negative<int> E(B);
 
-    auto C = add<int>(A, B);
+    auto C = nn.add<int, int, int>(nn.variable<int>(1), nn.variable<int>(2));
     //auto D = add<int>(negative<int>(B), add<int>(A, B));
 
     std::unordered_map<std::string, int *> feed = {};
-
-    Session<int> s;
-    int r = s.Run(C, feed);
+    int r = nn.run(C, feed);
 
     std::cout << r << std::endl;
 }
