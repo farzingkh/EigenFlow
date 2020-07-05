962c73c1c42ded21736c17e839f5216dae107db1
Farzin Golkhosh <farzin.golkhosh@gmail.com>
2020-07-04 18:16:32 -0700

    [FEAT] Update example for testing gradients

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,43 @@
 int main()
 {
 
-    typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> matxxf;
+    typedef Eigen::Matrix<long double, Eigen::Dynamic, Eigen::Dynamic> matxxf;
 
     NN nn = NN();
-    int const STEPS = 3;
+    int const STEPS = 2;
 
     // number of training samples
     int m = 1;
 
     // matrix of scalar 1
-    Eigen::Matrix<float, 1, 1> one;
+    Eigen::Matrix<long double, 1, 1> one;
     one << 1;
 
     // Bias (1*m)
-    Eigen::Matrix<float, 1, 1> b;
+    Eigen::Matrix<long double, 1, 1> b;
     b << 0;
 
     // Wieghts (nh*nx)
-    Eigen::Matrix<float, 1, 2> W;
+    Eigen::Matrix<long double, 1, 2> W;
     W << 1, 1;
 
     // Training Data (nx*m)
-    Eigen::Matrix<float, 2, 1> X;
+    Eigen::Matrix<long double, 2, 1> X;
     X << 3, 2;
     matxxf x = X;
 
     // Labels (nh*m)
-    Eigen::Matrix<float, 1, 1> Y;
+    Eigen::Matrix<long double, 1, 1> Y;
     Y << 1;
 
     // activation unit sigmoid(w^T*x+b) (nh*m)
-    BaseNode *G = nn.sigmoid<float>(nn.add<float>(nn.matmultiply<float>(nn.variable<float>(std::move(W)), nn.placeholder<float>("X")), nn.variable<float>(std::move(b))));
+    BaseNode *G = nn.sigmoid<long double>(nn.add<long double>(nn.matmultiply<long double>(nn.variable<long double>(std::move(W)), nn.placeholder<long double>("X")), nn.variable<long double>(std::move(b))));
 
     // create loss function -(y*log(a)+(1-y)*log(1-y))
-    //BaseNode* L = nn.negative<float>(nn.multiply<float>(nn.variable<float>(Y), nn.log<float>(G)));
+    BaseNode* L = nn.negative<long double>(nn.multiply<long double>(nn.variable<long double>(Y), nn.log<long double>(G)));
 
     // intermidiate loss function
-    BaseNode *L = nn.negative<float>(nn.add<float>(nn.multiply<float>(nn.variable<float>(Y), nn.log<float>(G)), nn.multiply<float>(nn.add<float>(nn.variable<float>(one), nn.negative<float>(nn.variable<float>(Y))), nn.log<float>(nn.add<float>(nn.variable<float>(one), nn.negative<float>(G))))));
+    //BaseNode *L = nn.negative<float>(nn.add<float>(nn.multiply<float>(nn.variable<float>(Y), nn.log<float>(G)), nn.multiply<float>(nn.add<float>(nn.variable<float>(one), nn.negative<float>(nn.variable<float>(Y))), nn.log<float>(nn.add<float>(nn.variable<float>(one), nn.negative<float>(G))))));
 
     // Create gradient descent optimization
     auto opt = GradientDescentOptimizer(0.01).minimize<matxxf>(L);
@@ -52,15 +52,16 @@ int main()
 
     // Run the session and get the value of the operation
     //matxxf g = nn.run<float>(G, feed);
-    nn.run<float>(L, feed);
+    nn.run<long double>(L, feed);
 
     for (int i = 1; i < STEPS; i++)
     {
-        nn.run<float>(&opt, feed);
-        nn.run<float>(L, feed);
-        std::cout << "Computational Operation Output for loss:" << std::endl;
-        std::cout << *(L->getValue<matxxf>()) << std::endl;
+        nn.run<long double>(&opt, feed);
+        nn.run<long double>(L, feed);
+        //std::cout << "Computational Operation Output for loss:" << std::endl;
+        //std::cout << *(L->getValue<matxxf>()) << std::endl;
     }
 
     // create minimization operation
+    nn.checkAllGradient(L,feed);
 }
