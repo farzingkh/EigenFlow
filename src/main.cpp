648e5cb95d497cbe74aab4b1310b03b236a29e0b
Farzin Golkhosh <farzin.golkhosh@gmail.com>
2020-07-10 17:42:17 -0700

    [CHORE] Update example and comments

--- src/main.cpp
+++ src/main.cpp
@@ -16,34 +16,36 @@ int main()
     Eigen::Matrix<long double, 1, 1> one;
     one << 1;
 
-    // Bias (1*m)
+    // Bias (m*1)
     Eigen::Matrix<long double, 1, 1> b;
-    b << 0;
+    b << 0.1;
 
     // Wieghts (nh*nx)
     Eigen::Matrix<long double, 1, 2> W;
-    W << 1, 1;
+    W << 0.1, 0.2;
 
     // Training Data (nx*m)
     Eigen::Matrix<long double, 2, 1> X;
     X << 3, 2;
     matxxf x = X;
 
-    // Labels (nh*m)
+    // Labels (1*m)
     Eigen::Matrix<long double, 1, 1> Y;
     Y << 1;
 
     // activation unit sigmoid(w^T*x+b) (nh*m)
     BaseNode *G = nn.sigmoid<long double>(nn.add<long double>(nn.matmultiply<long double>(nn.variable<long double>(std::move(W)), nn.placeholder<long double>("X")), nn.variable<long double>(std::move(b))));
 
+    //BaseNode *G = nn.sigmoid<long double>(nn.add<long double>(nn.matmultiply<long double>(nn.variable<long double>(std::move(W)), nn.placeholder<long double>("X")), nn.variable<long double>(std::move(b))));
+
     // create loss function -(y*log(a)+(1-y)*log(1-y))
-    BaseNode* L = nn.negative<long double>(nn.multiply<long double>(nn.variable<long double>(Y), nn.log<long double>(G)));
+    //BaseNode *L =  nn.negative<long double>((nn.log<long double>(G)));
 
     // intermidiate loss function
-    //BaseNode *L = nn.negative<float>(nn.add<float>(nn.multiply<float>(nn.variable<float>(Y), nn.log<float>(G)), nn.multiply<float>(nn.add<float>(nn.variable<float>(one), nn.negative<float>(nn.variable<float>(Y))), nn.log<float>(nn.add<float>(nn.variable<float>(one), nn.negative<float>(G))))));
+    BaseNode *L = nn.negative<long double>(nn.add<long double>(nn.matmultiply<long double>(nn.variable<long double>(Y), nn.log<long double>(G)), nn.matmultiply<long double>(nn.add<long double>(nn.variable<long double>(one), nn.negative<long double>(nn.variable<long double>(Y))), nn.log<long double>(nn.add<long double>(nn.variable<long double>(one), nn.negative<long double>(G))))));
 
     // Create gradient descent optimization
-    auto opt = GradientDescentOptimizer(0.01).minimize<matxxf>(L);
+    auto opt = GradientDescentOptimizer(0.0).minimize<matxxf>(L);
 
     // Create a map to feed data to the placeholders (i.e. X = X)
     std::unordered_map<std::string, matxxf *>
@@ -58,10 +60,10 @@ int main()
     {
         nn.run<long double>(&opt, feed);
         nn.run<long double>(L, feed);
-        //std::cout << "Computational Operation Output for loss:" << std::endl;
-        //std::cout << *(L->getValue<matxxf>()) << std::endl;
+        std::cout << "Computational Operation Output for loss:" << std::endl;
+        std::cout << *(L->getValue<matxxf>()) << std::endl;
     }
 
-    // create minimization operation
-    nn.checkAllGradient(L,feed);
+    /* Check gradients -- Make sure to set learning rate to zero befor checking!! -- */
+     nn.checkAllGradient(L, feed);
 }
