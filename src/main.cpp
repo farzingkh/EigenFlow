4535a91486adc6591726cc461666371ada968b36
Farzin Golkhosh <farzin.golkhosh@gmail.com>
2020-07-11 13:45:28 -0700

    [FIX] Use placeholders for cons nodes in example

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,30 @@ int main()
 
     typedef Eigen::Matrix<long double, Eigen::Dynamic, Eigen::Dynamic> matxxf;
 
+    // Initialize
     NN nn = NN();
-    int const STEPS = 2;
+    int const STEPS = 50;
 
     // number of training samples
     int m = 1;
+    
+    /* Use nn.spaceholders for constant variables, any nn.variables will be updated during optimization but placeholder values are kept constant */
 
     // matrix of scalar 1
-    Eigen::Matrix<long double, 1, 1> one;
-    one << 1;
+    Eigen::Matrix<long double, 1, 1> One;
+    One << 1;
+    matxxf n = One;
+    BaseNode* one = nn.placeholder<long double>("one");
 
     // Bias (m*1)
-    Eigen::Matrix<long double, 1, 1> b;
-    b << 0.1;
+    Eigen::Matrix<long double, 1, 1> B;
+    B << 0.1;
+    BaseNode* b = nn.variable<long double>(std::move(B));
 
     // Wieghts (nh*nx)
     Eigen::Matrix<long double, 1, 2> W;
     W << 0.1, 0.2;
+    BaseNode* w = nn.variable<long double>(std::move(W));
 
     // Training Data (nx*m)
     Eigen::Matrix<long double, 2, 1> X;
@@ -32,25 +39,25 @@ int main()
     // Labels (1*m)
     Eigen::Matrix<long double, 1, 1> Y;
     Y << 1;
+    matxxf yy = Y;
+    BaseNode* y = nn.placeholder<long double>("Y");
 
     // activation unit sigmoid(w^T*x+b) (nh*m)
-    BaseNode *G = nn.sigmoid<long double>(nn.add<long double>(nn.matmultiply<long double>(nn.variable<long double>(std::move(W)), nn.placeholder<long double>("X")), nn.variable<long double>(std::move(b))));
-
-    //BaseNode *G = nn.sigmoid<long double>(nn.add<long double>(nn.matmultiply<long double>(nn.variable<long double>(std::move(W)), nn.placeholder<long double>("X")), nn.variable<long double>(std::move(b))));
-
-    // create loss function -(y*log(a)+(1-y)*log(1-y))
-    //BaseNode *L =  nn.negative<long double>((nn.log<long double>(G)));
+    BaseNode *a = nn.sigmoid<long double>(nn.add<long double>(nn.matmultiply<long double>(w, nn.placeholder<long double>("X")), b));
 
     // intermidiate loss function
-    BaseNode *L = nn.negative<long double>(nn.add<long double>(nn.matmultiply<long double>(nn.variable<long double>(Y), nn.log<long double>(G)), nn.matmultiply<long double>(nn.add<long double>(nn.variable<long double>(one), nn.negative<long double>(nn.variable<long double>(Y))), nn.log<long double>(nn.add<long double>(nn.variable<long double>(one), nn.negative<long double>(G))))));
+    // create loss function -(y*log(a)+(1-y)*log(1-a))
+    BaseNode *L = nn.negative<long double>(nn.add<long double>(nn.matmultiply<long double>(y, nn.log<long double>(a)), nn.matmultiply<long double>(nn.add<long double>(one, nn.negative<long double>(y)), nn.log<long double>(nn.add<long double>(one, nn.negative<long double>(a))))));
 
     // Create gradient descent optimization
-    auto opt = GradientDescentOptimizer(0.0).minimize<matxxf>(L);
+    auto opt = GradientDescentOptimizer(0.01).minimize<matxxf>(L);
 
     // Create a map to feed data to the placeholders (i.e. X = X)
     std::unordered_map<std::string, matxxf *>
         feed = {};
     feed["X"] = &x;
+    feed["Y"] = &yy;
+    feed["one"] = &n;
 
     // Run the session and get the value of the operation
     //matxxf g = nn.run<float>(G, feed);
@@ -61,9 +68,12 @@ int main()
         nn.run<long double>(&opt, feed);
         nn.run<long double>(L, feed);
         std::cout << "Computational Operation Output for loss:" << std::endl;
-        std::cout << *(L->getValue<matxxf>()) << std::endl;
+        L->getValue<matxxf>();
+        w->getValue<matxxf>(); 
+        b->getValue<matxxf>();
+
     }
 
     /* Check gradients -- Make sure to set learning rate to zero befor checking!! -- */
-     nn.checkAllGradient(L, feed);
+    // nn.checkAllGradient(L, feed);
 }
