f0e14660a509234602484ecad07d0798352ed614
Farzin <farzin.golkhosh@gmail.com>
2020-07-21 22:43:53 -0700

    [FIX] Remove data members from gradient descent

--- include/gradientDescentOptimizer.h
+++ include/gradientDescentOptimizer.h
@@ -13,21 +13,15 @@ class GradientDescentOptimizer
 {
 public:
     GradientDescentOptimizer(float lr);
+    ~GradientDescentOptimizer() {};
     void computeGradients(BaseNode *loss);
-    // don't pass inputs by ref to avoid data race
-    std::vector<Locking_ptr<BaseNode>> getNodesList();
+
+    std::vector<Locking_ptr<BaseNode>> getNodeQueue(BaseNode *loss);
 
     template <typename T>
     Minimizer<T> minimize(BaseNode *loss);
 
     float learningRate_;
-    std::shared_ptr<std::mutex> Mtx_ = std::shared_ptr<std::mutex>(new std::mutex());
-
-private:
-    void getNodeQueue(BaseNode *loss);
-
-    std::vector<Locking_ptr<BaseNode>> NodesList_;
-    std::deque<Locking_ptr<BaseNode>> nodeQueue_;
 };
 
 #include "../src/gradientDescentOptimizer.tpp"
